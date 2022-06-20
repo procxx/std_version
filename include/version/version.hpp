23af4c8c1c34d2b81344d1c74e8f4b5e0e6c2ce4
shiz01 <shiz01@tutanota.com>
2022-06-20 15:45:43 +0700

    add clang-format form libcxx

--- include/version/version.hpp
+++ include/version/version.hpp
@@ -4,38 +4,32 @@
 #include <memory>
 
 #ifndef STD_VERSION_NS
-#define STD_VERSION_NS lbstd
+#  define STD_VERSION_NS lbstd
 #endif
 
 namespace STD_VERSION_NS {
 
 template <typename Impl>
-class version
-{
+class version {
 public:
-	Impl value_p; // intentionally public for constexprness
-	using value_type = Impl;
+  Impl value_p; // intentionally public for constexprness
+  using value_type = Impl;
 
-	constexpr version(value_type && t) noexcept : value_p{std::forward<value_type>(t)} {}
+  constexpr version(value_type&& t) noexcept : value_p{std::forward<value_type>(t)} {}
 
-	constexpr auto operator <=> (const version<Impl> &ver_) const = default;
+  constexpr auto operator<=>(const version<Impl>& ver_) const = default;
 
-	constexpr decltype(auto) value() const noexcept
-	{
-		return value_p;
-	}
+  constexpr decltype(auto) value() const noexcept { return value_p; }
 };
 
 template <typename Version>
-struct version_traits
-{
-	using value_type = typename Version::value_type;
+struct version_traits {
+  using value_type = typename Version::value_type;
 };
 
 template <typename Version1, typename Version2>
-constexpr auto operator <=> (const Version1 &ver_1, const Version2 &ver_2) noexcept
-{
-	return ver_1.value() <=> ver_2.value();
+constexpr auto operator<=>(const Version1& ver_1, const Version2& ver_2) noexcept {
+  return ver_1.value() <=> ver_2.value();
 }
 
 using simple_version = version<std::size_t>;
@@ -45,21 +39,18 @@ using simple_version = version<std::size_t>;
 namespace std {
 
 template <>
-struct tuple_size<::STD_VERSION_NS:: simple_version> : std::integral_constant<std::size_t, 1> {};
+struct tuple_size<::STD_VERSION_NS::simple_version> : std::integral_constant<std::size_t, 1> {};
 
 template <std::size_t I>
-struct tuple_element<I,::STD_VERSION_NS:: simple_version>
-{
-	static_assert(I < 2, "simple_version contains only one component");
-	using type = ::STD_VERSION_NS::version_traits<::STD_VERSION_NS::simple_version>::value_type;
+struct tuple_element<I, ::STD_VERSION_NS::simple_version> {
+  static_assert(I < 2, "simple_version contains only one component");
+  using type = ::STD_VERSION_NS::version_traits<::STD_VERSION_NS::simple_version>::value_type;
 };
 
 template <std::size_t I>
-constexpr std::size_t get(const ::STD_VERSION_NS::simple_version &ver) noexcept
-{
-	static_assert(I < std::tuple_size_v<::STD_VERSION_NS::simple_version>,
-			"simple_version contains only one component");
-	return ver.value();
+constexpr std::size_t get(const ::STD_VERSION_NS::simple_version& ver) noexcept {
+  static_assert(I < std::tuple_size_v<::STD_VERSION_NS::simple_version>, "simple_version contains only one component");
+  return ver.value();
 }
 
 } // namespace std
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,53 +4,38 @@
 
 #include <type_traits>
 
-#define GENERATE_COMPARISON_TEST_CASES(classname, val_less, val_eq, val_greater) \
-TEST_CASE("Comparison tests for " #classname) \
-{\
-	classname ver{(val_eq)}, ver_eq{(val_eq)}, ver_less{(val_less)}, ver_greater{(val_greater)}; \
-	SUBCASE("equal") \
-	{ \
-		REQUIRE_EQ(ver, ver_eq); \
-	} \
-	\
-	SUBCASE("not equal") \
-	{ \
-		REQUIRE_NE(ver, ver_greater); \
-	} \
-	\
-	SUBCASE("greater") \
-	{ \
-		REQUIRE(ver_greater > ver); \
-	} \
-	\
-	SUBCASE("less") \
-	{ \
-		REQUIRE(ver_less < ver); \
-	} \
-	\
-	SUBCASE("greater or equal") \
-	{ \
-		REQUIRE(ver >= ver_eq); \
-		REQUIRE(ver_greater >= ver); \
-	} \
-	\
-	SUBCASE("less or equal") \
-	{ \
-		REQUIRE(ver >= ver_less); \
-		REQUIRE(ver <= ver_eq); \
-	} \
-}
+#define GENERATE_COMPARISON_TEST_CASES(classname, val_less, val_eq, val_greater)                                       \
+  TEST_CASE("Comparison tests for " #classname) {                                                                      \
+    classname ver{(val_eq)}, ver_eq{(val_eq)}, ver_less{(val_less)}, ver_greater{(val_greater)};                       \
+    SUBCASE("equal") { REQUIRE_EQ(ver, ver_eq); }                                                                      \
+                                                                                                                       \
+    SUBCASE("not equal") { REQUIRE_NE(ver, ver_greater); }                                                             \
+                                                                                                                       \
+    SUBCASE("greater") { REQUIRE(ver_greater > ver); }                                                                 \
+                                                                                                                       \
+    SUBCASE("less") { REQUIRE(ver_less < ver); }                                                                       \
+                                                                                                                       \
+    SUBCASE("greater or equal") {                                                                                      \
+      REQUIRE(ver >= ver_eq);                                                                                          \
+      REQUIRE(ver_greater >= ver);                                                                                     \
+    }                                                                                                                  \
+                                                                                                                       \
+    SUBCASE("less or equal") {                                                                                         \
+      REQUIRE(ver >= ver_less);                                                                                        \
+      REQUIRE(ver <= ver_eq);                                                                                          \
+    }                                                                                                                  \
+  }
 
 GENERATE_COMPARISON_TEST_CASES(STD_VERSION_NS ::simple_version, 1, 2, 3)
 
-static_assert(std::is_same_v<::STD_VERSION_NS::version_traits<::STD_VERSION_NS::simple_version>::value_type, std::size_t>);
+static_assert(
+    std::is_same_v<::STD_VERSION_NS::version_traits<::STD_VERSION_NS::simple_version>::value_type, std::size_t>);
 
-TEST_CASE("Testing tuple interface of simple_version")
-{
-	using namespace STD_VERSION_NS;
-	constexpr simple_version ver{43};
-	static_assert(ver.value() == 43);
-	static_assert(std::get<0>(ver) == 43);
-	static_assert(std::tuple_size_v<simple_version> == 1);
-	static_assert(std::is_same_v<std::tuple_element_t<0, simple_version>, simple_version::value_type>);
+TEST_CASE("Testing tuple interface of simple_version") {
+  using namespace STD_VERSION_NS;
+  constexpr simple_version ver{43};
+  static_assert(ver.value() == 43);
+  static_assert(std::get<0>(ver) == 43);
+  static_assert(std::tuple_size_v<simple_version> == 1);
+  static_assert(std::is_same_v<std::tuple_element_t<0, simple_version>, simple_version::value_type>);
 }
